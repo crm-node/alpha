7ab029a588dc2d8644ade3eafe642ad2c4156d0b
msarukhanov <msarukhanov@gmail.com>
2016-08-14 14:34:16 +0500

    already refactoring :(

--- middleware/redisRequests.js
+++ middleware/redisRequests.js
@@ -5,89 +5,81 @@ var uuid = require('node-uuid');
 
 module.exports = {
 
-    storeData : function (customer_id, what, data, callback) {
-        if(!callback) callback = function (err, keys) {
-            if (err) return console.error(err);
-        };
+    customer : function (customer_id, what, data, callback) {
         switch (what) {
-            case 'customer':
-                data = JSON.stringify(data);
-                client.hset('customer:', customer_id, data, callback);
+            case 'add':
+                client.hset('customer:', customer_id, JSON.stringify(data.customer_info), callback);
                 break;
-            case 'clients-list-add':
-                data = JSON.stringify(data);
-                client.hset('customer:' + customer_id + ':clients:', "" + uuid.v4(), "" + data, callback);
+            case 'edit':
+                client.hset('customer:', data.customer_id, JSON.stringify(data.customer_info), callback);
                 break;
-            case 'user':
-                data = JSON.stringify(data);
-                client.hset('customer:' + customer_id + ':users:', "" + uuid.v4(), data, callback);
-                break;
-            case 'transactions':
-                data.user_id = JSON.stringify(data.user_id);
-                data.transaction = JSON.stringify(data.transaction);
-                client.hset('customer:' + customer_id + ':users:' + data.user_id + ':transaction:'  , "" + data.uuid.v4(), "" + data.transaction, callback);
-                break;
-            default :
-                callback({error: true, message: 'Invalid request'});
-                break;
-        }
-    },
-
-    getData : function(customer_id, what, data, callback) {
-        if(!callback) callback = function (err, keys) {
-            if (err) return console.error(err);
-        };
-        switch (what) {
-            case 'customer':
-                client.hget('customer:', customer_id, callback);
-                break;
-            case 'customers':
+            case 'all':
                 client.hgetall('customer:', callback);
                 break;
-            case 'clients-get':
-                client.hgetall('customer:' + customer_id + ':clients:', callback);
+            case 'get':
+                client.hget('customer:', customer_id, callback);
                 break;
-            case 'client-get':
-                client.hget('customer:' + customer_id + ':clients:', data.client_id, callback);
-                break;
-            case 'user':
-                client.hget('customer:' + customer_id + ':users:', data.user_id, callback);
-                break;
-            case 'users':
-                client.hgetall('customer:' + customer_id + ':users:', callback);
-                break;
-            case 'transactions':
-                client.hget('customer:' + customer_id + ':users:' + data.user_id + ':transaction:' + data.transaction_id, callback);
-                break;
-            default :
-                callback({error: true, message: 'Invalid request'});
+            case 'del':
+                client.hdel('customer:', customer_id, callback);
                 break;
         }
     },
 
-    deleteEvent : function(customer_id, what, data, callback) {
-        if(!callback) callback = function (err, keys) {
-            if (err) return console.error(err);
-        };
-        client.del('' + event_id, callback);
-        client.hdel('' + event_id + ':price:' + param + '', "" + object[0], callback);
+    user : function (customer_id, what, data, callback) {
         switch (what) {
-            case 'customer':
-                client.hdel('customer:', customer_id, callback);
+            case 'add':
+                client.hset('customer:' + customer_id + ':users:', "" + uuid.v4(), JSON.stringify(data.user_info), callback);
                 break;
-            case 'clients-list-get':
-                client.hdel('customer:' + customer_id + ':clients:', data.client_id, callback);
+            case 'edit':
+                client.hset('customer:' + customer_id + ':users:', "" + data.user_id, JSON.stringify(data.user_info), callback);
                 break;
-            case 'users':
+            case 'all':
+                client.hgetall('customer:' + customer_id + ':users:', callback);
+                break;
+            case 'get':
+                client.hget('customer:' + customer_id + ':users:', data.user_id, callback);
+                break;
+            case 'del':
                 client.hdel('customer:' + customer_id + ':users:', data.user_id, callback);
                 break;
-            case 'transactions':
-                client.hdel('customer:' + customer_id + ':users:' + data.user_id + ':transaction:', data.transaction_id, callback);
+        }
+    },
+
+    clients : function (customer_id, what, data, callback) {
+        switch (what) {
+            case 'add':
+                client.hset('customer:' + customer_id + ':clients:', "" + uuid.v4(), JSON.stringify(data.client_info), callback);
                 break;
-            default :
-                callback({error: true, message: 'Invalid request'});
+            case 'edit':
+                data = JSON.stringify(data);
+                client.hset('customer:' + customer_id + ':clients:', "" + data.client_id, "" + data, callback);
+                break;
+            case 'all':
+                client.hgetall('customer:' + customer_id + ':clients:', callback);
+                break;
+            case 'get':
+                client.hget('customer:' + customer_id + ':clients:', data.client_id, callback);
+                break;
+            case 'del':
+                client.hdel('customer:' + customer_id + ':clients:', data.client_id, callback);
+                break;
+        }
+    },
+    
+    transactions : function (customer_id, what, data, callback) {
+        switch (what) {
+            case 'add':
+                data.user_id = JSON.stringify(data.user_id);
+                data.transaction = JSON.stringify(data.transaction);
+                client.hset('customer:' + customer_id + ':users:' + data.user_id + ':transaction:'  , "" + uuid.v4(), JSON.stringify(data.transaction.info), callback);
+                break;
+            case 'get':
+                client.hget('customer:' + customer_id + ':users:' + data.user_id + ':transaction:' + data.transaction_id, callback);
+                break;
+            case 'del':
+                client.hdel('customer:' + customer_id + ':users:' + data.user_id + ':transaction:', data.transaction_id, callback);
                 break;
         }
     }
-
+    
 };
--- middleware/routes.js
+++ middleware/routes.js
@@ -13,68 +13,14 @@ var redisRequests = require('./redisRequests');
 //     }
 // });
 
-redisRequests.getData('admin', 'customers', {}, function (err, customer) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('customers : ', customer);
-    }
-});
-
-redisRequests.getData('admin', 'customer', {}, function (err, customer) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('customer : ', customer);
-    }
-});
-
-redisRequests.getData('admin', 'users', {}, function (err, users) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('users : ', users);
-    }
-});
-
-redisRequests.getData('admin', 'clients-get', {}, function (err, clients) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('clients : ', clients);
-    }
-});
-
-redisRequests.getData('admin', 'client-get', {client_id : 1}, function (err, clients) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('client : ', clients);
-    }
-});
-
-redisRequests.getData('admin', 'user', {user_id : 1}, function (err, userData) {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log('user : ', userData);
-    }
-});
-
 module.exports = function (app, fs) {
 
-    app.post('/api/login',function (req, res) {
+    app.post('/api/login', function (req, res) {
         if (req.headers.authorization != undefined) {
             res.send({error: true, message: 'Already logged in', error_code: 'auth_1'}).end();
         }
         else {
-            redisRequests.getData('admin', 'users', {}, function (err, users) {
+            redisRequests.user(req.body.customer, 'all', {}, function (err, users) {
                 if(err) res.send({error: true, message: 'Login error', error_code: 'auth_1'}).end();
                 else {
                     users = _.filter(JSON.parse(users), function(user){ 
@@ -93,24 +39,24 @@ module.exports = function (app, fs) {
         }
     });
 
-    app.post('/api/logout',function (req, res) {
-        dbRequest.getSports(function(callback){
-            res.send(callback).end();
-        });
+    app.post('/api/logout', function (req, res) {
+
     });
 
-    app.post('/api/getUsers',function (req, res) {
+
+
+    app.post('/api/getUsers', function (req, res) {
         if (req.headers.authorization == undefined) {
             res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
         }
         else {
-            redisRequests.getData(req.headers.authorization, 'users', function (err, userData) {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
                 if(err) {
                     res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
                 }
                 else {
                     userData = JSON.parse(userData);
-                    redisRequests.getData(userData.customer, 'users', {}, function (err, users) {
+                    redisRequests.user(userData.customer, 'all', {}, function (err, users) {
                         if(err) {
                             console.log(err);
                         }
@@ -130,24 +76,141 @@ module.exports = function (app, fs) {
         }
     });
 
-    app.post('/api/getUser',function (req, res) {
-        dbRequest.getSports(function(callback){
-            res.send(callback).end();
-        });
-    });
-    
-    app.post('/api/getClients',function (req, res) {
+    app.post('/api/getUser', function (req, res) {
         if (req.headers.authorization == undefined) {
             res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
         }
         else {
-            redisRequests.getData(req.headers.authorization, 'users', function (err, userData) {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.user(userData.customer, 'get', {user_id : req.body.user_id}, function (err, users) {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            users = JSON.parse(users);
+                            console.log(users);
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: users
+                            }).end();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    app.post('/api/editUser', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.user(userData.customer, 'edit', {user_id : req.body.user_id, user_info : req.body.user_info}, function (err, users) {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            users = JSON.parse(users);
+                            console.log(users);
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: users
+                            }).end();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    app.post('/api/addUser', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.user(userData.customer, 'add', {user_info : req.body.user_info}, function (err, users) {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            users = JSON.parse(users);
+                            console.log(users);
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: users
+                            }).end();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    app.post('/api/delUser', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.user(userData.customer, 'del', {user_id : req.body.user_id}, function (err, users) {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            users = JSON.parse(users);
+                            console.log(users);
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: users
+                            }).end();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+
+
+
+    app.post('/api/getClients', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
                if(err) {
                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
                }
                else {
                    userData = JSON.parse(userData);
-                   redisRequests.getData(userData.customer, 'clients-get', {}, function (err, cliensData) {
+                   redisRequests.clients(userData.customer, 'all', {}, function (err, cliensData) {
                        if(err) {
                            res.send({error: true, message: 'Clients request error', error_code: 'cli_1'}).end();
                        }
@@ -164,19 +227,125 @@ module.exports = function (app, fs) {
         }
     });
 
-    app.post('/api/getClient',function (req, res) {
-        redisRequests
+    app.post('/api/getClient', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.clients(userData.customer, 'get', {client_id : req.body.client_id}, function (err, cliensData) {
+                        if(err) {
+                            res.send({error: true, message: 'Clients request error', error_code: 'cli_1'}).end();
+                        }
+                        else {
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: JSON.parse(cliensData)
+                            }).end();
+                        }
+                    })
+                }
+            });
+        }
     });
 
-    app.post('/api/addClient',function (req, res) {
-        redisRequests
+    app.post('/api/editClient', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.clients(userData.customer, 'edit', {client_id : req.body.client_id, client_info : req.body.client_info}, function (err, cliensData) {
+                        if(err) {
+                            res.send({error: true, message: 'Clients request error', error_code: 'cli_1'}).end();
+                        }
+                        else {
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: JSON.parse(cliensData)
+                            }).end();
+                        }
+                    })
+                }
+            });
+        }
     });
 
-    app.post('/api/delClient',function (req, res) {
-        redisRequests
+    app.post('/api/addClient', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.clients(userData.customer, 'add', {client_info : req.body.client_info}, function (err, cliensData) {
+                        if(err) {
+                            res.send({error: true, message: 'Clients request error', error_code: 'cli_1'}).end();
+                        }
+                        else {
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: JSON.parse(cliensData)
+                            }).end();
+                        }
+                    })
+                }
+            });
+        }
     });
 
-    app.post('/api/getTransactions',function (req, res) {
+    app.post('/api/delClient', function (req, res) {
+        if (req.headers.authorization == undefined) {
+            res.send({error: true, message: 'Authorizatioin token required', error_code: 'auth_1'}).end();
+        }
+        else {
+            redisRequests.getUser(req.headers.authorization, function (err, userData) {
+                if(err) {
+                    res.send({error: true, message: "User doesn't exist", error_code: 'auth_1'}).end();
+                }
+                else {
+                    userData = JSON.parse(userData);
+                    redisRequests.clients(userData.customer, 'del', {client_id : req.body.client_id}, function (err, cliensData) {
+                        if(err) {
+                            res.send({error: true, message: 'Clients request error', error_code: 'cli_1'}).end();
+                        }
+                        else {
+                            res.send({
+                                error: false,
+                                message: 'Success',
+                                data: JSON.parse(cliensData)
+                            }).end();
+                        }
+                    })
+                }
+            });
+        }
+    });
+
+
+
+
+
+
+
+    app.post('/api/getTransactions', function (req, res) {
 
     });
 
