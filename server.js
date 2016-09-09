753964860bed0caca1eb90ac70fc1adb7cfaf1d4
msarukhanov <msarukhanov@gmail.com>
2016-09-09 18:10:09 +0500

    fix

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
